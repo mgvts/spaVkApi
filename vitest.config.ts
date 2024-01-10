b9f1ee5c9990d016b08e756b8fb8478bde168714
mgvts <89970331+mgvts@users.noreply.github.com>
2024-01-10 19:41:36 +0300

    some deploy changing

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,7 @@ import viteConfig from './vite.config'
 export default mergeConfig(
     viteConfig,
     defineConfig({
-        base: process.env.VITE_PROD_APP_URL,
+        base: '/spa_vk_api/',
         test: {
             environment: 'jsdom',
             exclude: [...configDefaults.exclude, 'e2e/*'],

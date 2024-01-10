017bc91413ca9ee225b4093e7584d89fcfde96c5
mgvts <89970331+mgvts@users.noreply.github.com>
2024-01-10 20:05:05 +0300

    deploy changing

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "./",
+    base: '/spa_vk_api/',
     plugins: [
         vue(),
         mkcert()
--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,6 @@ import viteConfig from './vite.config'
 export default mergeConfig(
     viteConfig,
     defineConfig({
-        base: '/spa_vk_api/',
         test: {
             environment: 'jsdom',
             exclude: [...configDefaults.exclude, 'e2e/*'],
